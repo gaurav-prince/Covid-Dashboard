1d1df7a4893d4407b7c93a95e64c08061bedffcf
Amiya Ranjan Das <amiya.ranjan.das@sap.com>
2021-05-08 01:22:13 +0530

    vaccination table fixed

--- src/app/vaccination/vaccination.component.ts
+++ src/app/vaccination/vaccination.component.ts
@@ -10,24 +10,28 @@ import { DatePipe } from '@angular/common'
 export class VaccinationComponent implements OnInit {
 
   constructor(private cscService: CscService, public datepipe: DatePipe) { }
-  // dataSource;
-  displayedColumns = [];
-  states: any = { states: []};
-  cities: any = { districts : []};
+
+  displayedColumns = ['name', 'address', 'district', 'age', 'time', 'fee_type', 'fee', 'date', 'available_capacity', 'vaccine'];
+  states: any = { states: [] };
+  cities: any = { districts: [] };
   vaccineSessions: any = {
-    sessions: []};
+    sessions: []
+  };
   dateString: string = '';
   pincode;
   cityId = '';
   loaded: boolean = false;
+  dataSource: any;
+
   ngOnInit(): void {
     this.cscService.getStates().subscribe(
       data => this.states = data
     );
   }
 
-  
+
   onChangeState(stateId: number) {
+    this.loaded=false;
     if (stateId) {
       this.cscService.getCities(stateId).subscribe(
         data => this.cities = data
@@ -38,41 +42,61 @@ export class VaccinationComponent implements OnInit {
   }
 
   onChangeDate(date) {
-    this.dateString =this.datepipe.transform(date, 'dd-MM-yyyy');
+    this.dateString = this.datepipe.transform(date, 'dd-MM-yyyy');
+    this.loaded=false;
   }
 
   onChangeCity(city: string) {
     this.cityId = city;
+    this.loaded=false;
   }
 
 
   onSubmitWithPin() {
     this.cscService.getVaccineByPin(this.pincode, this.dateString).subscribe(
-      data => this.vaccineSessions = data
+      data => {
+        this.vaccineSessions = data;
+        this.dataSource = this.vaccineSessions.sessions;
+      }
     );
     this.loaded = true;
   }
 
   onSubmitDistrict() {
     this.cscService.getVaccineByDistrict(this.cityId, this.dateString).subscribe(
-      data => this.vaccineSessions = data
+      data => {
+        this.vaccineSessions = data;
+        this.dataSource = this.vaccineSessions.sessions;
+      }
     );
     this.loaded = true;
   }
 
   onSubmitWithPinWeek() {
     this.cscService.getVaccineByPinWeek(this.pincode, this.dateString).subscribe(
-      data => this.vaccineSessions = data
+      data => {
+        this.vaccineSessions = data;
+        this.dataSource = this.vaccineSessions.sessions;
+      }
     );
     this.loaded = true;
   }
 
   onSubmitDistrictWeek() {
     this.cscService.getVaccineByDistrictWeek(this.cityId, this.dateString).subscribe(
-      data => this.vaccineSessions = data
+      data => {
+        this.vaccineSessions = data;
+        this.dataSource = this.vaccineSessions.sessions;
+      }
     );
     this.loaded = true;
   }
 
-  dataSource = this.vaccineSessions.sessions;
+  onTabChange() {
+    this.loaded = false;
+    this.vaccineSessions = {
+      sessions: []
+    };
+  }
+
 }

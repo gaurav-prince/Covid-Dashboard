5a2779fcc4004119067be74ea83d7ca2106cd9ca
Gaurav Prince <gkumar475@gmail.com>
2021-05-07 18:45:24 +0530

    Added country and state services

--- src/app/csc.service.ts
+++ src/app/csc.service.ts
@@ -1,30 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators'
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CscService {
-  apiBaseUrl = 'https://api.countrystatecity.in/v1/countries/[ciso]/states/[siso]/cities';
+  apiStateBaseUrl = 'https://cdn-api.co-vin.in/api/v2/admin/location/states';
+  apiCitiesBaseUrl = 'https://cdn-api.co-vin.in/api/v2/admin/location/districts'
 
   constructor(private http: HttpClient) { }
 
-  getCountries() {
-    return this.http.get(`${this.apiBaseUrl}api/countries`).pipe(
+  getStates() {
+    return this.http.get(`${this.apiStateBaseUrl}`).pipe(
       catchError(this.handleError)
     );
   }
 
-  getStates(countryId: number) {
-    return this.http.get(`${this.apiBaseUrl}api/states/${countryId}`).pipe(
-      catchError(this.handleError)
-    );
-  }
-
-  getCities(stateId: number) {
-    return this.http.get(`${this.apiBaseUrl}api/cities/${stateId}`).pipe(
+  getCities(state_id: number) {
+    return this.http.get(`${this.apiCitiesBaseUrl}/${state_id}`).pipe(
       catchError(this.handleError)
     );
   }
@@ -33,7 +28,8 @@ export class CscService {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error.message);
-    } else {
+    } 
+    else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
       console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
--- src/app/resourceform/resourceform.component.ts
+++ src/app/resourceform/resourceform.component.ts
@@ -11,61 +11,23 @@ export class ResourceformComponent implements OnInit {
   constructor(private cscService: CscService) { }
 
   title = 'Angular 4 Project!';
-  todaydate;
-  componentproperty;
-  emailid;
-  formdata;
-  createAccountForm: FormGroup;
-  countries: {};
-  states: {};
-  cities: {};
-  resources: [
+  states = {};
+  cities = {};
+  resources = [
     {id: 1, value: 'Oxygen Cylinder'}, 
     {id: 2, value: 'Oxygen Concentrator'},
     {id: 3, value: 'Blood Plasma'},
     {id: 4, value: 'Hospital Bed(General)'},
-    {id: 5, value: 'Hospital Bed(ICU)'}];
-  
+    {id: 5, value: 'Hospital Bed(ICU)'}
+  ];
+   
    ngOnInit() {
-      this.formdata = new FormGroup({
-         emailid: new FormControl("", Validators.compose([
-            Validators.required,
-            Validators.pattern("[^ @]*@[^ @]*")
-         ])),
-         passwd: new FormControl("", this.passwordvalidation)
-      });
-
-      this.cscService.getCountries().subscribe(
-        data => this.countries = data
-      );
-  
-      this.createAccountForm = new FormGroup({
-        country: new FormControl(''),
-        state: new FormControl(''),
-        city: new FormControl('')
-      });
-   }
-   passwordvalidation(formcontrol) {
-      if (formcontrol.value.length < 5) {
-         return {"passwd" : true};
-      }
+    this.cscService.getStates().subscribe(
+      data => this.states = data
+    );
    }
 
-   onChangeCountry(countryId: number) {
-      if (countryId) {
-        this.cscService.getStates(countryId).subscribe(
-          data => {
-            this.states = data;
-            this.cities = null;
-          }
-        );
-      } else {
-        this.states = null;
-        this.cities = null;
-      }
-    }
-
-    onChangeState(stateId: number) {
+   onChangeState(stateId: number) {
       if (stateId) {
         this.cscService.getCities(stateId).subscribe(
           data => this.cities = data
@@ -74,6 +36,4 @@ export class ResourceformComponent implements OnInit {
         this.cities = null;
       }
     }
-   loading = true;
-   onClickSubmit(data) {this.emailid = data.emailid;}
 }

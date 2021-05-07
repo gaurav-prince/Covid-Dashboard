326ea81a87157c32239e56bf06431154e84cb5b5
Amiya Ranjan Das <amiya.ranjan.das@sap.com>
2021-05-07 19:37:08 +0530

    few fixes

--- src/app/resourceform/resourceform.component.ts
+++ src/app/resourceform/resourceform.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, NgForm, FormGroupDirective} from '@angular/forms';
+import { FormGroup, FormControl, Validators, NgForm, FormGroupDirective } from '@angular/forms';
 import { CscService } from '../csc.service';
 import { ErrorStateMatcher } from '@angular/material/core';
 
@@ -20,36 +20,37 @@ export class ResourceformComponent implements OnInit {
   constructor(private cscService: CscService) { }
 
   title = 'Angular 4 Project!';
-  states: any = { states: []};
-  cities: any = { districts : []};
+  states: any = { states: [] };
+  cities: any = { districts: [] };
   resources = [
-    {id: 1, value: 'Oxygen Cylinder'}, 
-    {id: 2, value: 'Oxygen Concentrator'},
-    {id: 3, value: 'Blood Plasma'},
-    {id: 4, value: 'Hospital Bed(General)'},
-    {id: 5, value: 'Hospital Bed(ICU)'}
+    { id: 1, value: 'Oxygen Cylinder' },
+    { id: 2, value: 'Oxygen Concentrator' },
+    { id: 3, value: 'Blood Plasma' },
+    { id: 4, value: 'Hospital Bed(General)' },
+    { id: 5, value: 'Hospital Bed(ICU)' }
   ];
-   
-   ngOnInit() {
+
+  ngOnInit() {
     this.cscService.getStates().subscribe(
       data => this.states = data
     );
-   }
+  }
 
-   phoneFormControl = new FormControl('', [
+  phoneFormControl = new FormControl('', [
     Validators.pattern('[0-9]*'),
     Validators.minLength(10)
   ]);
-    onChangeState(stateId: number) {
-      console.log(stateId);
-      if (stateId) {
-        this.cscService.getCities(stateId).subscribe(
-          data => this.cities = data
-        );
-      } else {
-        this.cities = null;
-      }
+
+  onChangeState(stateId: number) {
+    console.log(stateId);
+    if (stateId) {
+      this.cscService.getCities(stateId).subscribe(
+        data => this.cities = data
+      );
+    } else {
+      this.cities = null;
     }
-    loading: false;
-    matcher = new MyErrorStateMatcher();
+  }
+  loading: false;
+  matcher = new MyErrorStateMatcher();
 }
